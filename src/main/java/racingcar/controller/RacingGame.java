bc408d49f1bd8446d0a805146ded21030b5d6341
mukeunzi <eunji057@gmail.com>
2022-04-25 19:27:34 +0900

    feat(controller): 유효하지 않은 입력부터 재입력받는 기능 추가
    - initializeCarGroup(), initializeRound() 메소드에 예외처리, while문을 사용해서 유효하지 않을 경우 재입력받도록 추가

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -4,6 +4,7 @@ import racingcar.domain.Car;
 import racingcar.domain.CarGroup;
 import racingcar.domain.CarName;
 import racingcar.domain.Round;
+import racingcar.exception.InvalidParameterError;
 import racingcar.view.Input;
 import racingcar.view.Print;
 
@@ -28,17 +29,41 @@ public class RacingGame {
     }
 
     public void initializeCarGroup() {
-        String inputCarNames = Input.inputCarNames();
+        while (this.carGroup == null) {
+            inputValidCarGroup();
+        }
+    }
 
+    public void inputValidCarGroup() {
+        try {
+            String inputCarNames = Input.inputCarNames();
+            List<Car> carNames = setCarNames(inputCarNames);
+            this.carGroup = new CarGroup(carNames);
+        } catch (InvalidParameterError e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public List<Car> setCarNames(String inputCarNames) {
         List<Car> carNames = new ArrayList<>();
         for (String carName : inputCarNames.trim().split(",")) {
             carNames.add(new Car(new CarName(carName.trim())));
         }
-        this.carGroup = new CarGroup(carNames);
+        return carNames;
     }
 
     public void initializeRound() {
-        String inputRound = Input.inputRound();
-        this.round = new Round(inputRound);
+        while (this.round == null) {
+            inputValidRound();
+        }
+    }
+
+    public void inputValidRound() {
+        try {
+            String inputRound = Input.inputRound();
+            this.round = new Round(inputRound);
+        } catch (InvalidParameterError e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
